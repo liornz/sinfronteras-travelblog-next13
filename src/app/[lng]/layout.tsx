90130dad77490971d8c6946676cc7d51cf878bf4
lior.nitzan <l.nitzan@synatix.com>
2026-01-24 13:58:52 +0100

    Add @vercel/speed-insights dependency and integrate into layout component

--- src/app/[lng]/layout.tsx
+++ src/app/[lng]/layout.tsx
@@ -1,8 +1,8 @@
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import { dir } from 'i18next';
 import type { Metadata, Viewport } from 'next';
 import { Lato } from 'next/font/google';
 import './globals.css';
-
 
 import Footer from '@/old_components/footer/footer';
 import SideDrawer from '@/old_components/layout/sideDrawer';
@@ -15,49 +15,50 @@ import '../../../styles/globals.scss';
 const lato = Lato({ subsets: ['latin'], weight: ['400', '700'] });
 
 export async function generateMetadata(): Promise<Metadata> {
-  return {
-    metadataBase: new URL('https://sinfronteras-travelblog.com/'),
-    title: 'Sin Fronteras - Video Travel Blog',
-    description:
-      'An Inspiring Video Travel Blog. I would like to inspire you and share with you my passion for culture and people from all over the world',
-    verification: {
-      google: 'l0w2h2JQOHdnGVdvB0UqUOHAon8XxS24IlkTFJOqXN0',
-    },
-    formatDetection: {
-      telephone: false,
-    },
-    manifest: '/manifest.json',
-  };
+    return {
+        metadataBase: new URL('https://sinfronteras-travelblog.com/'),
+        title: 'Sin Fronteras - Video Travel Blog',
+        description:
+            'An Inspiring Video Travel Blog. I would like to inspire you and share with you my passion for culture and people from all over the world',
+        verification: {
+            google: 'l0w2h2JQOHdnGVdvB0UqUOHAon8XxS24IlkTFJOqXN0',
+        },
+        formatDetection: {
+            telephone: false,
+        },
+        manifest: '/manifest.json',
+    };
 }
 
 export const viewport: Viewport = {
-  themeColor: 'black',
-  width: 'device-width',
-  initialScale: 1,
-  viewportFit: 'cover',
-  maximumScale: 1,
+    themeColor: 'black',
+    width: 'device-width',
+    initialScale: 1,
+    viewportFit: 'cover',
+    maximumScale: 1,
 };
 
 type RootLayoutProps = {
-  children: React.ReactNode;
-  params: Promise<{ lng: string }>;
+    children: React.ReactNode;
+    params: Promise<{ lng: string }>;
 };
 
 const drawerWidth = 240;
 
 export default async function RootLayout({ children, params }: RootLayoutProps) {
-  const { lng } = await params;
-  const countries = getAllCountriesData(lng);
-  const countriesData = countries.map((country) => ({ name: country.name, slug: country.slug }));
-  return (
-    <html lang={lng} dir={dir(lng)} suppressHydrationWarning>
-      <body className={lato.className}>
-        <SideDrawer lng={lng} countriesData={countriesData} drawerWidth={drawerWidth}>
-          {children}
-        </SideDrawer>
-        <Footer drawerWidth={drawerWidth} lng={lng} />
-        <ToastContainer />
-      </body>
-    </html>
-  );
+    const { lng } = await params;
+    const countries = getAllCountriesData(lng);
+    const countriesData = countries.map((country) => ({ name: country.name, slug: country.slug }));
+    return (
+        <html lang={lng} dir={dir(lng)} suppressHydrationWarning>
+            <body className={lato.className}>
+                <SideDrawer lng={lng} countriesData={countriesData} drawerWidth={drawerWidth}>
+                    {children}
+                </SideDrawer>
+                <Footer drawerWidth={drawerWidth} lng={lng} />
+                <ToastContainer />
+                <SpeedInsights />
+            </body>
+        </html>
+    );
 }
